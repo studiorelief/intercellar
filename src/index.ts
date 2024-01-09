6ae1cff0d9f0a11a164aac782e9e57baf0eaa4e6
Clément Murzeau <clement@studiorelief.fr>
2024-01-09 11:56:11 +0100

    build dist

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
 import './index.css';
 
-import { greetUser } from '$utils/greet';
+import { loadScript } from '$utils/loadscript';
+import { loadModelViewerScript } from '$utils/modalviewer';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name = 'John Doe';
-  greetUser(name);
+  // FS Attributes
+  Promise.all([
+    loadScript(
+      'https://cdn.jsdelivr.net/npm/@finsweet/attributes-scrolldisable@1/scrolldisable.js'
+    ),
+    loadScript('https://cdn.jsdelivr.net/npm/@finsweet/attributes-formsubmit@1/formsubmit.js'),
+  ]);
+
+  // Load 3D
+  loadModelViewerScript();
 });
